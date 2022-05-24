6a5aa6836f2640a4a56a06250ac681ec7f671ebc
crumb <81468461+CrumbGregory@users.noreply.github.com>
2022-05-24 16:50:59 +0500

    fixed some bugs

--- script.js
+++ script.js
@@ -1,41 +1,11 @@
-document.querySelector('button').onclick = myClick;
+document.querySelector('button').onclick = findChars;
 
-
-
-
-
-
-
-
-
-
-
-
-
-function myClick() {
+function findChars() {
 	let str = document.querySelector('.input').value;
-	let findA = str.match(/а/gi);
-	let findMOM = str.match(/мом/gi);
-	let resA = findA.length;
-	let resMOM = findMOM.length;
-	alert('я нашёл ' + resA + ' букв "А" и ' + resMOM + ' слогов "МОМ"');
-
-
+	let findA = str.match(/а/gi).length;
+	let findMOM = str.match(/мом/gi).length;
+	alert('я нашёл ' + findA + ' букв "А" и ' + findMOM + ' слогов "МОМ"');
 };
-
-(function () {
-	document.querySelector('.input').addEventListener('keydown', function (e) {
-		if (e.keyCode === 13) {
-			console.log(this.value);
-			let str = document.querySelector('.input').value;
-			let findA = str.match(/а/gi);
-			let findMOM = str.match(/мом/gi);
-			let resA = findA.length;
-			let resMOM = findMOM.length;
-			alert('я нашёл ' + resA + ' букв "А" и ' + resMOM + ' слогов "МОМ"');
-		}
-	});
-})();
 
 
 
